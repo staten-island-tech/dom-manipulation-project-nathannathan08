b1d04e71faf5f03707356397b1ad0bbec4ff380e
nathannathan08 <nathanc245@nycstudents.net>
2024-10-10 13:57:08 -0400

    KDD

---
+++ index.js
@@ -0,0 +1,50 @@
+const DOMSelectors = {
+
+    header: document.querySelector("h1"),
+    items: document.querySelectorAll("li"),
+    cardHeader: document.querySelector(".card-header"),
+    button: document.querySelector(".btn"),
+    container:document.querySelector(".container"),
+    form:document.querySelector(".form"),
+    };
+    
+    DOMSelectors.form.addEventListener("submit", function(event){
+    
+     event.preventDefault();
+     console.log(document.querySelectorAll("input").value);
+    
+    });
+//select all buttons as nodelist (can use for each)
+    const buttons = document.querySelectorAll("button");
+    //make an array from buttons if i want to use filterv etc.
+const newButtons = Array.from(buttons);
+//iterate through array and change each buttons color
+newButtons.forEach((button)=> (button.style.backgroundColor = "blue"));
+
+newButtons.forEach((button) => button.addEventListener("click", function (event) {
+    console.log(event.target.textContent);
+})
+);
+
+const inputs = [...document.querySelectorAll(".form input")];
+const gametitle = inputs[0].value;
+const gamerelease = inputs[1].value;
+const gameimage = inputs[2].value;
+
+
+DOMSelectors.container.insertAdjacentHTML(
+    "beforeend", 
+    `<div class="card">${gametitle}<h2 class="card-header">Some Text</h2></div>`
+    `<div class="card">${gamerelease}<h2 class="card-header">Some Text</h2></div>`
+    `<div class="card">${gameimage}<h2 class="card-img">Some Text</h2></div>`
+);
+
+//create the html for inputs, cards, and containers aka where card goes
+
+//select slash query in html form and get values from inputs
+
+//turn values into object Movie, Game, User, whatevs
+
+//insert card with object onto HTML
+
+//add event listener for remove button in js
